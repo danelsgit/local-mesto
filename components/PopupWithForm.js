72d80da0aba47145c89d8af4a4cfb12cd28357b6
dkanelka <140403233+danelsgit@users.noreply.github.com>
2025-06-17 22:19:36 +0300

    added remaining popup's logic

--- components/PopupWithForm.js
+++ components/PopupWithForm.js
@@ -1,5 +1,41 @@
 import Popup from "./Popup.js";
 
 export default class PopupWithForm extends Popup {
- 
+  constructor(popupSelector, formCallBack) {
+    super(popupSelector);
+    this._formCallBack = formCallBack;
+    this._form = this._popup.querySelector(".popup__form");
+    this._inputList = Array.from(this._form.querySelectorAll(".popup__input"));
+    this._sendButton = this._popup.querySelector(".popup__submit");
+    this._sendButtonText = this._sendButton.textContent;
+  }
+
+  _getInputValues() {
+    const inputValues = {};
+    this._inputList.forEach((input) => {
+      inputValues[input.name] = input.value;
+    });
+    return inputValues;
+  }
+
+  setButtonText() {
+    this._sendButton.textContent = "Сохранение...";
+  }
+
+  resetButtonText() {
+    this._sendButton.textContent = this._sendButtonText;
+  }
+
+  setEventListeners() {
+    super.setEventListeners();
+    this._form.addEventListener("submit", (evt) => {
+      evt.preventDefault();
+      this._formCallBack(this._getInputValues());
+    });
+  }
+
+  close() {
+    super.close();
+    this._form.reset();
+  }
 }
--- components/PopupWithImage.js
+++ components/PopupWithImage.js
@@ -1,5 +1,16 @@
 import Popup from "./Popup.js";
 
 export default class PopupWithImage extends Popup {
- 
+  constructor(popupSelector) {
+    super(popupSelector);
+    this._popupDescription = this._popup.querySelector(".popup__span");
+    this._popupImage = this._popup.querySelector(".popup__image");
+  }
+
+  open(name, link) {
+    super.open();
+    this._popupDescription.textContent = name;
+    this._popupImage.src = link;
+    this._popupImage.alt = name;
+  }
 }
